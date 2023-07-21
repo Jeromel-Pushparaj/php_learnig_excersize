2ffbc2c1c0f30acd65b72d94e7eb802e4fe6e408
jeromel <jeromal03@essentials>
2023-07-21 15:08:59 +0000

    21.07

---
+++ Practice/arrproduct.php
@@ -0,0 +1,9 @@
+<?php
+
+$int = array(1,5,64,73,7,9);
+
+//Now in the above array contains integer now we need that array procduct means we can use array_porduct()
+
+
+
+echo("The product of the array is " . array_product($int) . " <br> \n");
