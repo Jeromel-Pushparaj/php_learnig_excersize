2fda3131d3b9d6914340f0051329cf68ebff6ea7
jeromel <jeromal03@essentials>
2023-07-25 16:50:41 +0000

    old excersize

---
+++ php-learning/if.php
@@ -0,0 +1,25 @@
+<?php
+$bool1 = true;
+if ($bool1==true) {
+    ?>
+
+<head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+	<div class="card" style="width: 18rem;">
+		<img src="image.jpg" class="card-img-top" alt="...">
+		<div class="card-body">
+			<h5 class="card-title">Hey! I'm Jeromel</h5>
+			<p class="card-text">I am this competition conductor. I am so excited to se you all guy's in competition.
+			</p>
+			<a href="https://www.linkedin.com/in/jeromel-pushparaj-092a4b253?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BQp3sfCRzS7qwwOvHZSabdQ%3D%3D"
+				target="_blank" class="btn btn-primary">Know me</a>
+		</div>
+	</div>
+</body>
+<?php
+}
+?>
